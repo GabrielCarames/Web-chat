6a8e6b986c55fd5cd144334a79d00ee33e7eb06b
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-02-10 19:05:41 -0300

    verificaciones de inputs

--- routes/webchat.js
+++ routes/webchat.js
@@ -1,36 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Message = require('../models/message');
 const userController = require('../controllers/userController')
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 router.get('/', userController.isAuthenticated, function(req, res, next) {
   const cuenta = req.user
   console.log(cuenta.username)
   res.render('webchat', cuenta)
 });
-
-router.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
-
-router.post('/messages', async (req, res) => {
-  /*ya no sirve creo
-  const {message} = req.body;
-  const newMessage = new Message({message});
-  console.log('nuevomensaje')
-  await newMessage.save();
-  newMessage.save((err) =>{
-    if(err)
-      sendStatus(500);
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  })
-  */
-})
 
 module.exports = router;
