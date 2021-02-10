1d6e35a87be8691098d95edc3df026153f87cf2c
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-10 18:06:06 -0300

    nuevo campo imagen de perfil (avatar)

--- models/user.js
+++ models/user.js
@@ -10,6 +10,11 @@ const User = new Schema({
         type: String,
         required: true
     },
+    avatar: {
+        type: String,
+        required: false,
+        default: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fasia.ifoam.bio%2Fwp-content%2Fuploads%2F2018%2F12%2Favatar__181424.png&f=1&nofb=1',
+    },
     email: {
         type: String,
         required: true
