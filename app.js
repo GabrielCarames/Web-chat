3b437503566cdae8ce5822f4206c5aacfdbe2b8c
Manuel Cabral <cabral.manuel@yandex.com>
2021-02-10 22:10:59 -0300

    error handler flash

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 require("./db");
 
+// error handler
+app.use((req, res, next) => {
+  res.locals.messageSuccess = req.flash('messageSuccess');
+  res.locals.messageFailure = req.flash('messageFailure');
+  next();
+});
+
 // routes
 app.use('/', indexRouter);
 app.use('/user', userRouter);
@@ -55,19 +62,6 @@ app.use('/webchat', webchatRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.locals.messageFailure = req.flash('messageFailure');
-  
-  // render the error page
-  res.status(err.status || 500);
-  res.render('partials/error', {layout: false});
-  
 });
 
 
